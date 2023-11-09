17b54822c5301fe8a5e85a3891400239d8b26e15
Vitalii Kizlov <vitalii kizlov>
2023-11-09 20:27:03 +0100

    Add numberOfSubarraysProductLessThanK, bestSubArraysFixedWindowSize, findMaxAverageSubArray

--- MyPlayground.playground/Pages/Sliding Window.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Sliding Window.xcplaygroundpage/Contents.swift
@@ -53,4 +53,77 @@ func findLengthOfTheLongestSubstring(_ s: String) -> Int {
 let s = "1101100111"
 print(findLengthOfTheLongestSubstring(s))
 
+func numberOfSubarraysProductLessThanK(_ nums: [Int], _ k: Int) -> Int {
+    var left = 0
+    var ans = 0
+    var curr = 1
+
+    if k <= 1 { return 0 }
+
+    for right in 0..<nums.count {
+        curr *= nums[right]
+
+        while curr >= k {
+            curr /= nums[left]
+            left += 1
+        }
+
+        ans += right - left + 1
+    }
+
+    return ans
+}
+
+let subArraysNums = [10, 5, 2, 6]
+let subArraysK = 100
+print(numberOfSubarraysProductLessThanK(subArraysNums, subArraysK))
+
+func bestSubArraysFixedWindowSize(_ nums: [Int], _ k: Int) -> Int {
+    var ans = 0
+    var curr = 0
+
+    for i in 0..<k {
+        curr += nums[i]
+    }
+    ans = curr
+
+    for i in k..<nums.count {
+        curr += nums[i] - nums[i - k]
+        ans = max(ans, curr)
+    }
+
+    return ans
+}
+
+let fixedNums = [3, -1, 4, 12, -8, 5, 6]
+let fixedK = 4
+print(bestSubArraysFixedWindowSize(fixedNums, fixedK))
+
+func findMaxAverageSubArray(_ nums: [Int], _ k: Int) -> Double {
+    var ans: Double = 0
+    var curr: Double = 0
+
+    if k <= 1 {
+        for i in 0..<nums.count {
+            ans = max(ans, Double(nums[i]))
+        }
+    }
+
+    for i in 0..<k {
+        curr += Double(nums[i]) / Double(k)
+    }
+    ans = curr
+
+    for i in k..<nums.count {
+        curr += Double(nums[i] - nums[i - k]) / Double(k)
+        ans = max(ans, curr)
+    }
+
+    return ans
+}
+
+let maxAverageNums = [0, 4, 0, 3, 2]
+let maxAverageK = 1
+print(findMaxAverageSubArray(maxAverageNums, maxAverageK))
+
 //: [Next](@next)
